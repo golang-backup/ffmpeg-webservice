add17b7d4b2ac56e39a6e8d16f3f4f40ed90557e
Sven Patrick Meier <sven.patrick.meier@team-parallax.com>
2020-11-18 15:15:16 +0100

    adjust controller to match linting rules

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,30 +1,41 @@
+import { CapabilityService } from "../service/capabilities"
 import {
 	Controller,
 	Get,
 	Route,
 	Tags
 } from "tsoa"
-import { Inject } from "typescript-ioc"
 import {
 	ICodec,
 	IEncoder,
 	IFFmpegCapabilities,
 	IFilter,
-	IFormat,
+	IFormat
 } from "~/service/ffmpeg/interface"
-import { CapabilityService } from "../service/capabilities"
+import { Inject } from "typescript-ioc"
 @Route("/capabilities")
 @Tags("Capabilities")
 export class CapabilityController extends Controller {
 	@Inject
 	private readonly capabilityService!: CapabilityService
 	/**
-	 * Returns a list of capabilities and options that 
+	 * Returns a list of capabilities and options that
 	 * can be applied to an FFmpeg command.
 	 */
 	@Get("/")
 	public async getAvailableCapabilities(): Promise<IFFmpegCapabilities> {
 		return await this.capabilityService.getAllCapabilities()
+	}
+	@Get("/codecs")
+	public async getAvailableCodecs(): Promise<ICodec[]> {
+		return await this.capabilityService.getAvailableCodecs()
+	}
+	/**
+	 * Returns all available encoders FFmpeg is able to use.
+	 */
+	@Get("/encoders")
+	public async getAvailableEncoders(): Promise<IEncoder[]> {
+		return await this.capabilityService.getAvailableEncoders()
 	}
 	/**
 	 * Returns all available filters that can be used for conversion.
@@ -39,29 +50,5 @@ export class CapabilityController extends Controller {
 	@Get("/formats")
 	public async getAvailableFormats(): Promise<IFormat[]> {
 		return await this.capabilityService.getAvailableFormats()
-	}
-	
-	@Get("/codecs")
-	public async getAvailableCodecs(): Promise<ICodec[]> {
-		return await this.capabilityService.getAvailableCodecs()
-		// catch (error) {
-		// 	return {
-		// 		message: error.message,
-		// 		status: error.code
-		// 	}
-		// }
-	}
-	/**
-	 * Returns all available encoders FFmpeg is able to use.
-	 */
-	@Get("/encoders")
-	public async getAvailableEncoders(): Promise<IEncoder[]> {
-		return await this.capabilityService.getAvailableEncoders()
-		// catch (error) {
-		// 	return {
-		// 		message: error.message,
-		// 		status: error.code
-		// 	}
-		// }
 	}
 }
