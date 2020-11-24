8cd778f5d74dc2079e12377499e02a3d9e1a24d0
Sven Patrick Meier <sven.patrick.meier@team-parallax.com>
2020-11-24 11:15:48 +0100

    Add beforeAll function to initialize test setup

--- src/tests/ffmpegWrapper.spec.ts
+++ src/tests/ffmpegWrapper.spec.ts
@@ -1,4 +1,16 @@
 import { FFmpegWrapper } from "../service/ffmpeg"
+import { basePath } from "../constants"
+import { createDirectoryIfNotPresent } from "../service/file-io"
+import path from "path"
+beforeAll(async (done: jest.DoneCallback) => {
+	const paths = ["input", "output"]
+	const dirPromises: Promise<string>[] = []
+	for (const dirPath of paths) {
+		dirPromises.push(createDirectoryIfNotPresent(path.join(basePath, dirPath)))
+	}
+	const resp = await Promise.all(dirPromises)
+	done()
+})
 describe("FFmpegWrapper should pass all tests", () => {
 	const ffmpeg: FFmpegWrapper = new FFmpegWrapper()
 	it("should convert .mp3 to .mp4", async (done: jest.DoneCallback) => {
